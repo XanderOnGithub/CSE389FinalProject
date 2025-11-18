dc17ba1194f145f9dffcb4a39ac828108ae78908
Xander <xanderreyes2005@gmail.com>
2025-11-18 11:03:12 -0500

    Added Basic Input Functionality to Character

--- Source/CSE389FinalProject/Characters/PlayerCharacter.cpp
+++ Source/CSE389FinalProject/Characters/PlayerCharacter.cpp
@@ -1,11 +1,115 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PlayerCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+
+    // Default Tick Stuff
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Allow FPS Turning
+    bUseControllerRotationYaw = true;
+    
+    // Create spring arm
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+    SpringArmComponent->SetupAttachment(GetRootComponent());
+    SpringArmComponent->TargetArmLength = 0.0f;
+    SpringArmComponent->bUsePawnControlRotation = true; 
+    SpringArmComponent->bInheritPitch = false;
+    SpringArmComponent->bInheritRoll = false;
+    SpringArmComponent->bInheritYaw = false;
+
+    // Create camera
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+    CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+    CameraComponent->bUsePawnControlRotation = false; 
+    
+}
+
+
+
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+    APlayerController* PC = Cast<APlayerController>(GetController());
+
+    if (PC)
+    {
+        UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+        if (Subsystem)
+        {
+            Subsystem->ClearAllMappings();
+            Subsystem->AddMappingContext(InputMapping, 0);
+        }
+    }
+
+    UEnhancedInputComponent* InputComp = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+    
+    if (InputComp)
+    {
+        InputComp->BindAction(InputMove, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
+        InputComp->BindAction(InputLook, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+        InputComp->BindAction(InputSprint, ETriggerEvent::Triggered, this, &APlayerCharacter::Sprint);
+        InputComp->BindAction(InputInteract, ETriggerEvent::Triggered, this, &APlayerCharacter::DoInteract);
+        InputComp->BindAction(InputJump, ETriggerEvent::Triggered, this, &APlayerCharacter::DoJump);
+    }
+}
+
+/**
+ * Character Actions
+ * Including: Move, Look, Jump, Sprint, Interact
+ */
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+    // Take Movement Value
+    FVector2D MovementVector = Value.Get<FVector2D>();
+
+    if (MovementVector.X != 0)
+    {
+       AddMovementInput(GetActorRightVector(), MovementVector.X);
+    }
+
+    if (MovementVector.Y != 0)
+    {
+       AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+    }
+}
+
+void APlayerCharacter::Look(const FInputActionValue& Value)
+{
+    FVector2D LookDirection = Value.Get<FVector2D>();
+
+    if (LookDirection.X != 0)
+    {
+       AddControllerYawInput(LookDirection.X);
+    }
+
+    if (LookDirection.Y != 0)
+    {
+       AddControllerPitchInput(LookDirection.Y);
+    }
+}
+
+void APlayerCharacter::Sprint(const FInputActionValue& Value)
+{
+    UE_LOG(LogTemp, Display, TEXT("sprint"));
+}
+
+void APlayerCharacter::DoInteract(const FInputActionValue& Value)
+{
+    UE_LOG(LogTemp, Display, TEXT("DoInteract"));
+}
+
+void APlayerCharacter::DoJump(const FInputActionValue& Value)
+{
+    bool JumpValue = Value.Get<bool>();
+    if (JumpValue && CanJump())
+    {
+       Jump();
+    }
 }
--- Source/CSE389FinalProject/Characters/PlayerCharacter.h
+++ Source/CSE389FinalProject/Characters/PlayerCharacter.h
@@ -4,22 +4,58 @@
 
 #include "CoreMinimal.h"
 #include "BaseCharacter.h"
+#include "InputActionValue.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "PlayerCharacter.generated.h"
 
 /**
- * 
- */
+ * */
 UCLASS()
 class CSE389FINALPROJECT_API APlayerCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
 
 public:
+	APlayerCharacter();
 
 protected:
-	
-	
-	public:
-    	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputMappingContext* InputMapping;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* InputMove;
+    
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* InputLook;
+    
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* InputSprint;   
+    
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* InputJump;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	class UInputAction* InputInteract;
+    
+	void Move(const FInputActionValue& Value);
+    
+	void Look(const FInputActionValue& Value);
+    
+	void Sprint(const FInputActionValue& Value);
+
+	void DoInteract(const FInputActionValue& Value);
+
+	void DoJump(const FInputActionValue& Value);
+
+    
+public:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Components")
+	UCameraComponent* CameraComponent;
 };
